048a8262d6336aa0aea701026fc9511815a85d6b
Madhav singla <55372575+Madhavsingla1@users.noreply.github.com>
2022-07-03 02:43:40 +0530

    Time: 6 ms (19.74%), Space: 5.7 MB (88.29%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) 
     {
-        long long num=1;
-        while(n>num)
-        {
-            num=num<<1;
-        }
-        return num==n;
+        // long long num=1;
+        // while(n>num)
+        // {
+        //     num=num<<1;
+        // }
+        // return num==n;
+        if(n<=0)return 0;
+        return !(n&(n-1));
     }
 };
