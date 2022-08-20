2f747d99d14c3bbac4338cf3486dbfad39e2c977
Madhav singla <55372575+Madhavsingla1@users.noreply.github.com>
2022-08-20 13:37:06 +0530

    Time: 21 ms (44.09%), Space: 22.3 MB (6.53%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,24 +9,39 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+struct Three{
+    long long mx;
+    long long mn;
+    bool ans;
+};
 class Solution {
-    bool res=1;
+    // bool res=1;
 public:
-    void solve(TreeNode* root, long long &a)
+    // void solve(TreeNode* root, long long &a)
+    // {
+    //     if(!root)return ;
+    //     solve(root->left,a);
+    //     if(a>=root->val)
+    //     {
+    //         res=0;
+    //         return;
+    //     }
+    //     a=root->val;
+    //     solve(root->right,a);
+    // }
+    Three solve(TreeNode* root)
     {
-        if(!root)return ;
-        solve(root->left,a);
-        if(a>=root->val)
-        {
-            res=0;
-            return;
-        }
-        a=root->val;
-        solve(root->right,a);
+        if(!root)return {INT64_MIN,INT64_MAX,true};
+        Three l = solve(root->left);
+        Three r = solve(root->right);
+        Three ret;
+        ret.ans = l.ans and r.ans and l.mx<root->val and root->val<r.mn;
+        ret.mx = max({r.mx,l.mx,(long long)root->val});
+        ret.mn = min({r.mn,l.mn,(long long)root->val});
+        return ret;
     }
     bool isValidBST(TreeNode* root) {
-        long long a = -INT64_MAX;
-        solve(root,a);
-        return res;
+        Three d = solve(root);
+        return d.ans;
     }
 };
