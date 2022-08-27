87f9928b99a12d1c693203bc69f715b1a6c8d2de
Madhav singla <55372575+Madhavsingla1@users.noreply.github.com>
2022-08-27 22:28:49 +0530

    Time: 22 ms (25.66%), Space: 7.3 MB (46.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-     sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+     // sort(s.begin(),s.end());
+     //    sort(t.begin(),t.end());
+     //    return s==t;
+        unordered_map<char,int>mp;
+        for(auto i:s)
+            mp[i]++;
+        for(auto i:t)
+        {
+            mp[i]--;
+            if(mp[i]==0)mp.erase(i);
+        }
+        return !mp.size();
     }
 };
