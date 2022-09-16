fe121bd60a2ad798d3acd58c2ffc9c9e5b610587
Madhav singla <55372575+Madhavsingla1@users.noreply.github.com>
2022-09-16 21:25:19 +0530

    Time: 1095 ms (42.93%), Space: 253.3 MB (5.71%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int minGroups(vector<vector<int>>& in) 
     {
-        vector<int>vec(1e7+1,0);
+        vector<int>vec(1000005,0);
         int mx = INT_MIN;
         for(auto i:in)
         {
             vec[i[0]]++;
             vec[i[1]+1]--;
-            mx = max(mx,i[1]+1);
         }
         
         int res = 0, sum=0;
-        for(int i=0;i<mx+1;i++)
+        for(int i=0;i<1000005;i++)
         {
             sum+=vec[i];
             res =max(sum,res);
